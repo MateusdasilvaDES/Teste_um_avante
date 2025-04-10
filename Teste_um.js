b0d0fa49b456e09a05d3ab84d2aa73c211ed0411
MateusdasilvaDES <mateus_silva44@estudante.sesisenai>
2025-04-10 14:04:48 -0300

    novas mudanças carrocel

---
+++ Teste_um.js
@@ -0,0 +1,168 @@
+const produtos = [
+    {
+      nome: "Camiseta  Branca",
+      precoOriginal: 100,
+      precoAtual: 79.9,
+      parcelas: "10x de R$ 7,90",
+      imagem: "images/imagem_carrocel.svg",
+      desconto: "10% OFF",
+      
+    },
+    {
+      nome: "Camiseta branca",
+      precoOriginal: 100,
+      precoAtual: 79.9,
+      parcelas: "10x de R$ 7,90",
+      imagem: "images/imagem_carrocel.svg",
+      desconto: "10% OFF",
+      
+    },
+    {
+      nome: "Camiseta Branca",
+      precoOriginal: 100,
+      precoAtual: 79.9,
+      parcelas: "10x de R$ 7,90",
+      imagem: "images/imagem_carrocel.svg",
+      desconto: "10% OFF",
+      
+    },
+    {
+      nome: "Camiseta Branca",
+      precoOriginal: 100,
+      precoAtual: 79.9,
+      parcelas: "10x de R$ 7,90",
+      imagem: "images/imagem_carrocel.svg",
+      desconto: "10% OFF",
+      
+    },
+    {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+        
+      },
+      {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+        
+      },
+      {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+        
+      },
+      {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+        
+      },
+      {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+        
+      },
+      {
+        nome: "Camiseta Branca",
+        precoOriginal: 100,
+        precoAtual: 79.9,
+        parcelas: "10x de R$ 7,90",
+        imagem: "images/imagem_carrocel.svg",
+        desconto: "10% OFF",
+    
+      },
+      {
+          nome: "Camiseta Branca",
+          precoOriginal: 100,
+          precoAtual: 79.9,
+          parcelas: "10x de R$ 7,90",
+          imagem: "images/imagem_carrocel.svg",
+          desconto: "10% OFF",
+       
+        },
+        {
+          nome: "Camiseta branca",
+          precoOriginal: 100,
+          precoAtual: 79.9,
+          parcelas: "10x de R$ 7,90",
+          imagem: "images/imagem_carrocel.svg",
+          desconto: "10% OFF",
+          
+        },
+        {
+            nome: "Camiseta Branca",
+            precoOriginal: 100,
+            precoAtual: 79.9,
+            parcelas: "10x de R$ 7,90",
+            imagem: "images/imagem_carrocel.svg",
+            desconto: "10% OFF",
+            
+          },
+          {
+              nome: "Camiseta Branca",
+              precoOriginal: 100,
+              precoAtual: 79.9,
+              parcelas: "10x de R$ 7,90",
+              imagem: "images/imagem_carrocel.svg",
+              desconto: "10% OFF",
+              
+            },
+            {
+              nome: "Camiseta branca",
+              precoOriginal: 100,
+              precoAtual: 79.9,
+              parcelas: "10x de R$ 7,90",
+              imagem: "images/imagem_carrocel.svg",
+              desconto: "10% OFF",
+              
+            }
+  ];
+
+
+  /* Linha de codigo para estudar map com implementção de div no HTML
+  
+  window.addEventListener("DOMContentLoaded", () => {
+
+  const usuarios = [
+    { nome: "Alice", idade: 25 },
+    { nome: "Bruno", idade: 30 },
+    { nome: "Carla", idade: 22 }
+  ];
+
+  const container = document.getElementById("container");
+
+  usuarios.map(usuario => {
+    const div = document.createElement("div");
+    div.classList.add("caixa");
+
+    div.innerHTML = `
+      <h3>${usuario.nome}</h3>
+      <p>Idade: ${usuario.idade} anos</p>
+    `;
+
+    container.appendChild(div);
+  });
+});
+
+*/
+
+
+  
